76d476cbc4816e4e8e525dc5737a6f184246a77e
James Nguyen <kevinusa2010@yahoo.com>
2020-04-20 20:00:47 -0700

    Updates

---
+++ precache-manifest.2efb4ef1d518c2a0c32b6174d37700a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73f0cba84d9413903441",
+    "url": "/course/static/css/main.bcf893c5.chunk.css"
+  },
+  {
+    "revision": "73f0cba84d9413903441",
+    "url": "/course/static/js/main.73f0cba8.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "71ac93c7336517a5f6b6",
+    "url": "/course/static/js/runtime~main.71ac93c7.js"
+  },
+  {
+    "revision": "5e88420b6680b1c11e47ce28d6ab01f2",
+    "url": "/course/index.html"
+  }
+];
